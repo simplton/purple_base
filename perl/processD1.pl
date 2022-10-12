04696b814508271572471b15dad68137061ed5cd
Formatted <ronivegh@gmail.com>
2022-10-12 14:24:28 -0600

    added in processor

---
+++ perl/processD1.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+open FILE, "Day1input";
+my @shifts;
+my $SUM = 0;
+my %frequencys;
+$SUM = 0;
+
+while  (my  $line  =  <FILE>)  {
+  push @shifts, $line;
+}
+
+$frequencys{$SUM} = 11;
+while (1) {
+  for (@shifts) {
+    $SUM += $_;
+    if (exists($frequencys{$SUM})) {
+      print "$SUM\n";
+      exit 0;
+    } else {
+      $frequencys{$SUM} = 11;
+    }
+  }
+}
